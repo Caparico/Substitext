4bdce23786112e4b0b5e335deca93020b140dc0e
caparico <kobi.birdman@gmail.com>
2013-01-15 19:17:11 +0200

    added mouse hover responsiveness.

--- substitext.js
+++ substitext.js
@@ -1,5 +1,11 @@
 jQuery.fn.substitext = function() {
 	return this.each(function() {
-		jQuery(this).text("Changed text");
+		var originalText = jQuery(this).text();
+
+		jQuery(this).hover(function(){
+			jQuery(this).text("Zeine mouse hovered!");
+		}, function() {
+			jQuery(this).text(originalText);
+		});
 	});
 };
